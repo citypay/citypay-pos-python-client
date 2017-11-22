df21205504c5b5bd75b5ac5e2a34fb9430f7e4db
Gary Feltham <gary.feltham@citypay.com>
2017-11-22 13:40:50 +0000

    initial import

--- citypay/api_client.py
+++ citypay/api_client.py
@@ -2,7 +2,7 @@
 """
     CityPay POS API
 
-    CityPay Point of Sale API for payment with card present devices including EMV readers and contactless POS readers.  The API makes it simple to add EMV and contactless card acceptance to iOS, Android, Tablet and desktop applicaitons using a HTTPS protocol. It segregates the complexity of payment processing from the sales environment and eliminates any necessity for the target system to handle card data.   # noqa: E501
+    CityPay Point of Sale API for payment with card present devices including EMV readers and contactless POS readers.  The API is available from https://github.com/citypay/citypay-pos-api  The API makes it simple to add EMV and contactless card acceptance to iOS, Android, Tablet and desktop applicaitons using a HTTPS protocol. It segregates the complexity of payment processing from the sales environment and eliminates any necessity for the target system to handle card data.   # noqa: E501
 
     OpenAPI spec version: 1.0.0
     Contact: dev@citypay.com
